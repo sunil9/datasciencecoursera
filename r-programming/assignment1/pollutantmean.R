b2aa375f37798212bb6c6298c49df35f5ce3af40
Sunil Pereira <sunil.per.us@gmail.com>
2015-04-19 15:10:15 -0400

    Part 1 - Commit

--- r-programming/assignment1/pollutantmean.R
+++ r-programming/assignment1/pollutantmean.R
@@ -18,31 +18,39 @@ pollutantmean <- function(directory, pollutant, id = 1:332) {
     # The mean of the pollutant across all monitors list in the 'id' vector (ignoring NA values)
     
     # begin ----------------------------------------------------- 
-    # read entire file list (monitors) from 'directory' into character vector 
-    monitors.list <- list.files(directory)
-    # creates subset of monitors from the above list as specified by 'id'
-    monitors.subset <- monitors.list[id]
     
-    #initialize the pollutant dataset to be read from each monitor csv
-    pollutant.dataset <- NA
-    
-    # read monitors subset into pollutant dataset
-    if(length(monitors.subset) >= 1) {
-        for (file.csv in monitors.subset) {   
-            monitor.file <- paste(directory,"/", file.csv, sep = '')
-           pollutant.dataset <- rbind(pollutant.dataset, read.csv(monitor.file))
-        } 
-        
-        #calculate mean for dataset with column name = pollutant
-        pollutant.mean <- mean(pollutant.dataset[,pollutant], na.rm = TRUE)
-        
-        # round the mean to 3 decimal digits
-        round(pollutant.mean, digits=3)
+    # check if argument 'id' is is valid numeric range and not character 
+    pollutant.vector <- c("sulfate","nitrate")
+    if (id < 1 || id > 332 || !is.numeric(id) || is.na(match(pollutant,pollutant.vector))) {
+        print("Error: invalid argument type")
+        print("Args:pollutant = sulfate or nitrate")
+        print("Args:id should be in the range of 1:332")       
+        print("Usage: pollutantmean(directory, pollutant, id = 1:332) ")
     } else {
         
-        "Warning: Monitor file not found"
+        # read entire file list (monitors) from 'directory' into character vector 
+        monitors.list <- list.files(directory)
+        # creates subset of monitors from the above list as specified by 'id'
+        monitors.subset <- monitors.list[id]
         
-    }      
+        #initialize the pollutant dataset to be read from each monitor csv
+        pollutant.dataset <- NA
+        
+        # read monitors subset into pollutant dataset
+        if(length(monitors.subset) >= 1) {
+            for (filename in monitors.subset) {   
+                monitor.file <- paste(directory,"/", filename, sep = '')
+                pollutant.dataset <- rbind(pollutant.dataset, read.csv(monitor.file))
+            } 
+            
+            #calculate mean for dataset with column name = pollutant
+            pollutant.mean <- mean(pollutant.dataset[,pollutant], na.rm = TRUE)
+            
+            # round the mean to 3 decimal digits
+            round(pollutant.mean, digits=3)
+            
+        } 
+    }
 }
 
 
