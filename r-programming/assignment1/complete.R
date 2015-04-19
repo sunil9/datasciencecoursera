a0ef4a74affd57889706473076828ae1fa340f5c
Sunil Pereira <sunil.per.us@gmail.com>
2015-04-19 15:10:26 -0400

    Part 2 - Commit

--- r-programming/assignment1/complete.R
+++ r-programming/assignment1/complete.R
@@ -22,7 +22,33 @@ complete <- function(directory, id = 1:332) {
     # ...
     # where 'id' is the monitor ID number and 'nobs' is the
     # number of complete cases
-   
+    
     # begin ----------------------------------------------------- 
     
+    # check if argument 'id' is is valid numeric range and not character 
+    if (id < 1 || id > 332 || !is.numeric(id)) {
+        print("Error: id - invalid argument range/type")
+        print("Usage: complete(directory, id = 1:332)")
+    } else {
+                
+        # read entire file list (monitors) from 'directory' into character vector 
+        monitors.list <- list.files(directory)
+        # creates subset of monitors from the above list as specified by 'id'
+        monitors.subset <- monitors.list[id]
+        
+        #Initialize the vector to record complete cases
+        complete.observations = numeric()
+        
+        # read monitors subset and computes the total of complete cases and
+        # adds to complete.oberservations vector 
+        
+        for (filename in monitors.subset) {   
+            monitor.file <- paste(directory,"/", filename, sep = '')
+            total.cases <- sum(complete.cases(read.csv(monitor.file)))
+            complete.observations <- c(complete.observations, total.cases)            
+        } 
+        
+        # creates data frame with column headers: id, nobs(complete.observations) 
+        data.frame(id, nobs=complete.observations)
+    }
 }    
