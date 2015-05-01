c61a0182d694b92a33636bfc9e291d10bedfb533
Sunil Pereira <sunil.per.us@gmail.com>
2015-05-01 08:18:30 -0400

    R Programming Templates

---
+++ r-programming/_Templates.R/sunils_codeTemplate.R
@@ -0,0 +1,19 @@
+# header ----------------------------------------------------------------------
+# Course: R Programming
+# Programming Assignment 2: Lexical Scoping
+# Author: Sunil Pereira
+# Created Date: April 26, 2015
+
+# sampleFunction() ------------------------------------------------------------
+# << function description .. >> 
+# Args:
+#   arg1: << details of arg1 
+#         e.g. id: integer vector indicating the monitor ID numbers to be used>> 
+#   arg2: << details of arg2 >>
+# Returns:
+# << What does the function return? >>
+
+sampleFunction <- function(arg1, arg2) {
+    
+}
+   
